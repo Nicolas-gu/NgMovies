47bb40d5aa15424171385e1fa4fd424c8b38aae8
Nicolas G <nicolas.guebenne@edu.technoble.be>
2025-08-24 16:53:00 +0200

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './ui/home/home';
 import { MyFavorite } from './ui/my-favorite/my-favorite';
 import { ReactivForm } from './ui/reactiv-form/reactiv-form';
+import { Movies } from './ui/movies/movies';
 
 export const routes: Routes = [
     {path: "", redirectTo: 'home', pathMatch: 'full' },
     {path: "home", component: Home},
     {path: "my_favorite", component: MyFavorite},
     {path: "new-user", component: ReactivForm},
+    {path: "top_rated", component: Movies},
 ];
--- src/app/ui/movies/movies-list/movies-list.ts
+++ src/app/ui/movies/movies-list/movies-list.ts
@@ -1,20 +1,24 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { ApiService } from '../../../core/services/api-service';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './movies-list.html',
   styleUrl: './movies-list.scss'
 })
 export class MoviesList implements OnInit{
 
   api = inject(ApiService);
+  _router = inject(Router)
   moviesList = signal<any[]>([]);
-  category = "/top_rated"
+  // category = "/top_rated"
+  category = input.required<string>();
   
   ngOnInit(): void {
-    this.api.getMovieListByCategory(this.category).subscribe(
+    this.api.getMovieListByCategory(this.category()).subscribe(
       data => {
         this.moviesList.set(data.results)
         console.log(this.moviesList)
